2f2969da0b352221c48399b9f39aa1aca8528eba
usonia09 <uwasonia2001@gmail.com>
2024-03-13 14:57:03 -0400

    layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<nav>
+	{#each pages as p }
+    <a href={p.url}>{p.title}</a>
+	{/each}
+</nav> <slot />
+<script>
+  import { linear } from "svelte/easing";
+
+    let pages = [
+    { url: "./", title: "Home" },
+    {
+        url: "./projects",
+        title: "Projects",
+    },
+    {
+        url: "./Assignments",
+        title: "Assignments",
+    },
+    {
+        url: "./contact",
+        title: "Contact Me",
+    },
+    {
+        url: "./resume",
+        title: "My Resume",
+    },
+    ];
+
+</script>
+
+
