9e9dec145478c82f90a48389c1f1afe4d39ee48b
usonia09 <uwasonia2001@gmail.com>
2024-03-21 17:54:06 -0400

    step 3 completed

--- src/lib/Pie.svelte
+++ src/lib/Pie.svelte
@@ -2,14 +2,7 @@
     import * as d3 from 'd3';
 
     let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-    let data = [
-	{ value: 1, label: "apples" },
-	{ value: 2, label: "oranges" },
-	{ value: 3, label: "mangos" },
-	{ value: 4, label: "pears" },
-	{ value: 5, label: "limes" },
-	{ value: 5, label: "cherries" }
-    ];
+    export let data = [];
     let colors = d3.scaleOrdinal(d3.schemeTableau10);
     let sliceGenerator = d3.pie().value(d => d.value);
     let arcData = sliceGenerator(data);
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,12 +2,19 @@
   import projects from '$lib/projects.json';
   import Project from '../../lib/Project.svelte';
   import Pie from '../../lib/Pie.svelte';
+  import * as d3 from 'd3';
+
+  let rolledData = d3.rollups(projects, v => v.length, d => d.year);
+  let pieData = rolledData.map(([year, count]) => {
+	return { value: count, label: year };
+});
+
 </script>
 <svelte:head>
 	<title>Projects</title>
 </svelte:head>
 <h1>{projects.length} Projects</h1>
-<Pie/>
+<Pie data={pieData}/>
 <div class="projects">
 {#each projects as p}
 <Project info={p} hLevel=3/>
